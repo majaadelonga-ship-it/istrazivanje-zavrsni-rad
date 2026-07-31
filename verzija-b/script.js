031a38f4899071bb44dd39d51ac5e6f61cebfc9e
majaadelonga-ship-it <majaa.delonga@gmail.com>
2026-07-31 20:35:01 +0200

    Update script.js

--- verzija-b/script.js
+++ verzija-b/script.js
@@ -1,7 +1,9 @@
+
+
 const EXPOSURE_TIME_SECONDS = 15;
 
-const GOOGLE_FORM_BASE = "https://docs.google.com/forms/d/e/1FAIpQLSdd7dqRbQc9rAXnuldpGfuwVG5i14yQWECWvkmdeObzpEA-EQ/viewform";
-const GROUP_ENTRY_ID = "entry.2037905476";
+const GOOGLE_FORM_BASE = "https://docs.google.com/forms/d/e/1FAIpQLScmCnnW2vt0z6ngGWDnn7yqmy88d5PO2ymCgi7iJsq6NyGdCQ/viewform";
+const GROUP_ENTRY_ID = "entry.918774680";
 const GROUP_VALUE = "B"; // ova stranica = verzija B (niska kognitivna lakoća)
 
 const SURVEY_URL = `${GOOGLE_FORM_BASE}?usp=pp_url&${GROUP_ENTRY_ID}=${GROUP_VALUE}`;
