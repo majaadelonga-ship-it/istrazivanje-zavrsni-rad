3610c98f9a8dbd6ca7ebf02645a2c711133891d3
majaadelonga-ship-it <majaa.delonga@gmail.com>
2026-08-04 10:30:23 +0200

    Update script.js

--- verzija-a/script.js
+++ verzija-a/script.js
@@ -1,9 +1,8 @@
 
+const EXPOSURE_TIME_SECONDS = 20;
 
-const EXPOSURE_TIME_SECONDS = 15;
-
-const GOOGLE_FORM_BASE = "https://docs.google.com/forms/d/e/1FAIpQLScmCnnW2vt0z6ngGWDnn7yqmy88d5PO2ymCgi7iJsq6NyGdCQ/viewform";
-const GROUP_ENTRY_ID = "entry.918774680";
+const GOOGLE_FORM_BASE = "https://docs.google.com/forms/d/e/1FAIpQLSdd7dqRbQc9rAXnuldpGfuwVG5i14yQWECWvkmdeObzpEA-EQ/viewform";
+const GROUP_ENTRY_ID = "entry.2037905476";
 const GROUP_VALUE = "A"; // ova stranica = verzija A (visoka kognitivna lakoća)
 
 const SURVEY_URL = `${GOOGLE_FORM_BASE}?usp=pp_url&${GROUP_ENTRY_ID}=${GROUP_VALUE}`;
@@ -15,7 +14,7 @@ requestAnimationFrame(() => {
   timerBar.style.transform = "scaleX(0)";
 });
 
-// Automatsko preusmjeravanje nakon 15 sekundi na pilot anketu, s već upisanom grupom.
+// Automatsko preusmjeravanje nakon 20 sekundi na anketu, s već upisanom grupom.
 setTimeout(() => {
   if (SURVEY_URL) {
     window.location.href = SURVEY_URL;
